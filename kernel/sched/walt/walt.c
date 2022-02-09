1bb3c41d686406dfdd1fdcac9d8744e2f3e54a71
Shaleen Agrawal <quic_shalagra@quicinc.com>
2022-02-09 11:30:06 -0800

    sched: walt: Introduce bucket bitmask
    In get_pred_busy, we attempt to find the closing bucket, which is
    nothing but the first bucket which is filled after the start bucket.
    
    We can optimize this computation by using a bitmask of buckets.
    
    Change-Id: I377be499fdd4876d5d3a43e8ebb34a489e6ae17b
    Signed-off-by: Shaleen Agrawal <quic_shalagra@quicinc.com>
    [dereference23: Backport to msm-5.4]
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/sched/walt/walt.c
+++ kernel/sched/walt/walt.c
@@ -1123,7 +1123,7 @@ unsigned int sysctl_sched_many_wakeup_threshold = WALT_MANY_WAKEUP_DEFAULT;
  * decayed. The rate of increase and decay could be different based
  * on current count in the bucket.
  */
-static inline void bucket_increase(u8 *buckets, int idx)
+static inline void bucket_increase(u8 *buckets, u16 *bucket_bitmask, int idx)
 {
 	int i, step;
 
@@ -1131,8 +1131,10 @@ static inline void bucket_increase(u8 *buckets, int idx)
 		if (idx != i) {
 			if (buckets[i] > DEC_STEP)
 				buckets[i] -= DEC_STEP;
-			else
+			else {
 				buckets[i] = 0;
+				*bucket_bitmask &= ~BIT_MASK(i);
+			}
 		} else {
 			step = buckets[i] >= CONSISTENT_THRES ?
 						INC_STEP_BIG : INC_STEP;
@@ -1140,6 +1142,7 @@ static inline void bucket_increase(u8 *buckets, int idx)
 				buckets[i] = U8_MAX;
 			else
 				buckets[i] += step;
+			*bucket_bitmask |= BIT_MASK(i);
 		}
 	}
 }
@@ -1179,26 +1182,22 @@ static inline int busy_to_bucket(u32 normalized_rt)
  * to use for prediction. Once found, it returns the midpoint of that bucket.
  */
 static u32 get_pred_busy(struct task_struct *p,
-				int start, u32 runtime)
+				int start, u32 runtime, u16 bucket_bitmask)
 {
-	int i;
-	u8 *buckets = p->wts.busy_buckets;
 	u32 dmin, dmax;
 	u64 cur_freq_runtime = 0;
-	int first = NUM_BUSY_BUCKETS, final;
+	int first = NUM_BUSY_BUCKETS, final = NUM_BUSY_BUCKETS;
 	u32 ret = runtime;
+	u16 next_mask = bucket_bitmask >> start;
 
 	/* skip prediction for new tasks due to lack of history */
 	if (unlikely(is_new_task(p)))
 		goto out;
 
 	/* find minimal bucket index to pick */
-	for (i = start; i < NUM_BUSY_BUCKETS; i++) {
-		if (buckets[i]) {
-			first = i;
-			break;
-		}
-	}
+	if (next_mask)
+		first = ffs(next_mask) - 1 + start;
+
 	/* if no higher buckets are filled, predict runtime */
 	if (first >= NUM_BUSY_BUCKETS)
 		goto out;
@@ -1234,7 +1233,7 @@ static inline u32 calc_pred_demand(struct task_struct *p)
 		return p->wts.pred_demand;
 
 	return get_pred_busy(p, busy_to_bucket(p->wts.curr_window),
-			     p->wts.curr_window);
+			     p->wts.curr_window, p->wts.bucket_bitmask);
 }
 
 /*
@@ -1795,8 +1794,8 @@ static inline u32 predict_and_update_buckets(
 		return 0;
 
 	bidx = busy_to_bucket(runtime);
-	pred_demand = get_pred_busy(p, bidx, runtime);
-	bucket_increase(p->wts.busy_buckets, bidx);
+	pred_demand = get_pred_busy(p, bidx, runtime, p->wts.bucket_bitmask);
+	bucket_increase(p->wts.busy_buckets, &p->wts.bucket_bitmask, bidx);
 
 	return pred_demand;
 }
@@ -2205,6 +2204,7 @@ void init_new_task_load(struct task_struct *p)
 	for (i = 0; i < NUM_BUSY_BUCKETS; ++i)
 		p->wts.busy_buckets[i] = 0;
 
+	p->wts.bucket_bitmask = 0;
 	p->wts.cpu_cycles = 0;
 	memset(&p->wts.curr_window_cpu, 0, sizeof(u32) * nr_cpu_ids);
 	memset(&p->wts.prev_window_cpu, 0, sizeof(u32) * nr_cpu_ids);
@@ -2255,6 +2255,7 @@ void reset_task_stats(struct task_struct *p)
 	p->wts.pred_demand_scaled = 0;
 	p->wts.active_time = 0;
 	p->wts.cidx = 0;
+	p->wts.bucket_bitmask = 0;
 }
 
 void mark_task_starting(struct task_struct *p)
