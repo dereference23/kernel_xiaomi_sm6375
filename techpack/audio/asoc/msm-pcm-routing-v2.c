6bf7c14b68991592c794c2a23dc74913125e4731
Marijn Suijten <marijn.suijten@somainline.org>
2022-05-07 15:34:02 +0200

    asoc: Don't check if a `static` array is `NULL` (`-Werror=address`)
    A `static` array (in the `.bss` section of a typical executable) always
    has a location in memory: it makes no sense to ever check for a `NULL`
    here.  Since this array holds pointers it was perhaps "intended" to
    check individual fields of this array before dereferencing, which we now
    do inside the `msm_routing_find_topology_on_index()` function.
    
    (Other code in this file sets/unsets `cal_data`, but it's not
     zero-initialized though...)
    
    Change-Id: Ib30d111bf13436cac14e6932eeea99781172a3e2
    Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>

--- techpack/audio/asoc/msm-pcm-routing-v2.c
+++ techpack/audio/asoc/msm-pcm-routing-v2.c
@@ -1876,6 +1876,9 @@ static int msm_routing_find_topology_on_index(int fedai_id, int session_type, in
 	int topology = -EINVAL;
 	struct cal_block_data *cal_block = NULL;
 
+	if (cal_data[idx] == NULL)
+		return topology;
+
 	mutex_lock(&cal_data[idx]->lock);
 	if (idx == ADM_TOPOLOGY_CAL_TYPE_IDX)
 		cal_block = msm_routing_find_topology_by_buf_number(fedai_id,
@@ -1909,9 +1912,6 @@ static int msm_routing_get_adm_topology(int fedai_id, int session_type,
 	pr_debug("%s: fedai_id %d, session_type %d, be_id %d\n",
 	       __func__, fedai_id, session_type, be_id);
 
-	if (cal_data == NULL)
-		goto done;
-
 	app_type = fe_dai_app_type_cfg[fedai_id][session_type][be_id].app_type;
 	acdb_dev_id =
 		fe_dai_app_type_cfg[fedai_id][session_type][be_id].acdb_dev_id;
@@ -1934,7 +1934,7 @@ static int msm_routing_get_adm_topology(int fedai_id, int session_type,
 		if (topology < 0)
 			topology = NULL_COPP_TOPOLOGY;
 	}
-done:
+
 	pr_debug("%s: Using topology %d\n", __func__, topology);
 	return topology;
 }
