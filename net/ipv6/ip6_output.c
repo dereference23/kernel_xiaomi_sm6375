81e2e600bf77d30d776e3f05a4c59a704cfee9bf
Alexander Winkowski <dereference23@outlook.com>
2023-02-04 14:50:57 +0000

    net: Don't reduce TTL/HL
    Some mobile carriers compare TTL/HL values to restrict
    tethering. Just skip the decrement to make it harder for them.
    
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -532,6 +532,7 @@ int ip6_forward(struct sk_buff *skb)
 			return 0;
 	}
 
+#if 0
 	/*
 	 *	check and decrement ttl
 	 */
@@ -544,6 +545,7 @@ int ip6_forward(struct sk_buff *skb)
 		kfree_skb(skb);
 		return -ETIMEDOUT;
 	}
+#endif
 
 	/* XXX: idev->cnf.proxy_ndp? */
 	if (net->ipv6.devconf_all->proxy_ndp &&
@@ -632,7 +634,9 @@ int ip6_forward(struct sk_buff *skb)
 
 	/* Mangling hops number delayed to point after skb COW */
 
+#if 0
 	hdr->hop_limit--;
+#endif
 
 	return NF_HOOK(NFPROTO_IPV6, NF_INET_FORWARD,
 		       net, NULL, skb, skb->dev, dst->dev,
