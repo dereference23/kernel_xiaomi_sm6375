b985983bc3d0ef2b2275ba13a0b87ddf7934cc62
Jamie Iles <quic_jiles@quicinc.com>
2023-06-13 15:19:59 +0100

    arm64/mm: remove now-superfluous ISBs from TTBR writes
    At the time of authoring 7655abb95386 ("arm64: mm: Move ASID from TTBR0
    to TTBR1"), the Arm ARM did not specify any ordering guarantees for
    direct writes to TTBR0_ELx and TTBR1_ELx and so an ISB was required
    after each write to ensure TLBs would only be populated from the
    expected (or reserved tables).
    
    In a recent update to the Arm ARM, the requirements have been relaxed to
    reflect the implementation of current CPUs and required implementation
    of future CPUs to read (RDYDPX in D8.2.3 Translation table base address
    register):
    
      Direct writes to TTBR0_ELx and TTBR1_ELx occur in program order
      relative to one another, without the need for explicit
      synchronization. For any one translation, all indirect reads of
      TTBR0_ELx and TTBR1_ELx that are made as part of the translation
      observe only one point in that order of direct writes.
    
    Remove the superfluous ISBs to optimize uaccess helpers and context
    switch.
    
    Cc: Will Deacon <will@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Jamie Iles <quic_jiles@quicinc.com>
    Reviewed-by: Mark Rutland <mark.rutland@arm.com>
    Link: https://lore.kernel.org/r/20230613141959.92697-1-quic_jiles@quicinc.com
    [catalin.marinas@arm.com: rename __cpu_set_reserved_ttbr0 to ..._nosync]
    [catalin.marinas@arm.com: move the cpu_set_reserved_ttbr0_nosync() call to cpu_do_switch_mm()]
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/include/asm/uaccess.h
+++ arch/arm64/include/asm/uaccess.h
@@ -114,7 +114,6 @@ static inline void __uaccess_ttbr0_disable(void)
 	ttbr &= ~TTBR_ASID_MASK;
 	/* reserved_pg_dir placed before swapper_pg_dir */
 	write_sysreg(ttbr - PAGE_SIZE, ttbr0_el1);
-	isb();
 	/* Set reserved ASID */
 	write_sysreg(ttbr, ttbr1_el1);
 	isb();
@@ -138,7 +137,6 @@ static inline void __uaccess_ttbr0_enable(void)
 	ttbr1 &= ~TTBR_ASID_MASK;		/* safety measure */
 	ttbr1 |= ttbr0 & TTBR_ASID_MASK;
 	write_sysreg(ttbr1, ttbr1_el1);
-	isb();
 
 	/* Restore user page table */
 	write_sysreg(ttbr0, ttbr0_el1);
