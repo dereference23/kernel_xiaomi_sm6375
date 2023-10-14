d7b6091695761baa509435215368b618f1830e2b
Alexander Winkowski <dereference23@outlook.com>
2023-10-14 19:35:09 +0000

    Revert "binder: update low_latency selection for binder transactions"
    This reverts commit acfaa3c73b95e0f4db0249a2146752bf77ccc8df.
    
    Change-Id: Ia82b93f76b44e821c7f13d42bb5bb97071745c64
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -543,8 +543,7 @@ static void binder_wakeup_poll_threads_ilocked(struct binder_proc *proc,
 #ifdef CONFIG_SCHED_WALT
 			if (thread->task && current->signal &&
 				(current->signal->oom_score_adj == 0) &&
-				((current->prio < DEFAULT_PRIO) ||
-					(thread->task->group_leader->prio < MAX_RT_PRIO)))
+				(current->prio < DEFAULT_PRIO))
 				thread->task->wts.low_latency |=
 						WALT_LOW_LATENCY_BINDER;
 #endif
@@ -610,8 +609,7 @@ static void binder_wakeup_thread_ilocked(struct binder_proc *proc,
 #ifdef CONFIG_SCHED_WALT
 		if (thread->task && current->signal &&
 			(current->signal->oom_score_adj == 0) &&
-			((current->prio < DEFAULT_PRIO) ||
-				(thread->task->group_leader->prio < MAX_RT_PRIO)))
+			(current->prio < DEFAULT_PRIO))
 			thread->task->wts.low_latency |=
 					WALT_LOW_LATENCY_BINDER;
 #endif
