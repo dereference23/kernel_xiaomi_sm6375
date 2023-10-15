83c0b5fd7ed95ea70c0d4fa67d86c44317bac403
Alexander Winkowski <dereference23@outlook.com>
2023-10-15 06:19:57 +0000

    Revert "sched/fair: Don't place wakee on waker cpu if colocate enabled"
    This reverts commit e24b1df8d98f33ff236eba8ea1ea7c8990f622eb.
    
    Change-Id: Ibb6d18579f70ebf973614757056798e43ad6b342
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7185,7 +7185,7 @@ int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu,
 	struct perf_domain *pd;
 	unsigned long cur_energy;
 	cpumask_t *candidates;
-	bool is_rtg, curr_is_rtg;
+	bool is_rtg;
 	struct find_best_target_env fbt_env;
 	bool need_idle = wake_to_idle(p);
 	int placement_boost = task_boost_policy(p);
@@ -7199,7 +7199,6 @@ int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu,
 		goto eas_not_ready;
 
 	is_rtg = task_in_related_thread_group(p);
-	curr_is_rtg = task_in_related_thread_group(cpu_rq(cpu)->curr);
 
 	fbt_env.fastpath = 0;
 
@@ -7210,7 +7209,7 @@ int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu,
 	candidates = this_cpu_ptr(&energy_cpus);
 	cpumask_clear(candidates);
 
-	if (sync && (need_idle || (is_rtg && curr_is_rtg)))
+	if (need_idle)
 		sync = 0;
 
 	if (sync && bias_to_this_cpu(p, cpu, start_cpu)) {
