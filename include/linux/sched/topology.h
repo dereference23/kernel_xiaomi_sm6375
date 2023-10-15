540703f1b869c5dfbdc9cee39922942ec0a0c441
Alexander Winkowski <dereference23@outlook.com>
2023-10-15 07:30:50 +0000

    Revert "ANDROID: sched: Per-Sched-domain over utilization"
    This reverts commit de785069ecc7ebd63bf1f3b9448409f4fc59adb4.
    
    Change-Id: I2a2b834ee190b06a8e5aafb6ee5ffc1ee0723b23
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- include/linux/sched/topology.h
+++ include/linux/sched/topology.h
@@ -67,10 +67,6 @@ struct sched_domain_shared {
 	atomic_t	ref;
 	atomic_t	nr_busy_cpus;
 	int		has_idle_cores;
-
-#ifdef CONFIG_SCHED_WALT
-	bool            overutilized;
-#endif
 };
 
 struct sched_domain {
