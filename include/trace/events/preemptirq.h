4049f8d2c7e612e4d1f4d7bcbb56e599a011d564
Alexander Winkowski <dereference23@outlook.com>
2023-10-15 06:05:30 +0000

    Revert "trace: increase caller depth for preempt/irqs disable events"
    This reverts commit 9f2b8d8b1a205bbaec8fda38aad1aca12a5e15ba.
    
    Change-Id: I7b33a2656006957676d4aa0f982886db14cf959f
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- include/trace/events/preemptirq.h
+++ include/trace/events/preemptirq.h
@@ -65,10 +65,9 @@ DEFINE_EVENT(preemptirq_template, preempt_enable,
 TRACE_EVENT(irqs_disable,
 
 	TP_PROTO(u64 delta, unsigned long caddr0, unsigned long caddr1,
-				unsigned long caddr2, unsigned long caddr3,
-				unsigned long caddr4),
+				unsigned long caddr2, unsigned long caddr3),
 
-	TP_ARGS(delta, caddr0, caddr1, caddr2, caddr3, caddr4),
+	TP_ARGS(delta, caddr0, caddr1, caddr2, caddr3),
 
 	TP_STRUCT__entry(
 		__field(u64, delta)
@@ -76,7 +75,6 @@ TRACE_EVENT(irqs_disable,
 		__field(void*, caddr1)
 		__field(void*, caddr2)
 		__field(void*, caddr3)
-		__field(void*, caddr4)
 	),
 
 	TP_fast_assign(
@@ -85,22 +83,20 @@ TRACE_EVENT(irqs_disable,
 		__entry->caddr1 = (void *)caddr1;
 		__entry->caddr2 = (void *)caddr2;
 		__entry->caddr3 = (void *)caddr3;
-		__entry->caddr4 = (void *)caddr4;
 	),
 
-	TP_printk("delta=%llu(ns) Callers:(%ps<-%ps<-%ps<-%ps<-%ps)",
-					__entry->delta, __entry->caddr0,
-					__entry->caddr1, __entry->caddr2,
-					__entry->caddr3, __entry->caddr4)
+	TP_printk("delta=%llu(ns) Callers:(%ps<-%ps<-%ps<-%ps)", __entry->delta,
+					__entry->caddr0, __entry->caddr1,
+					__entry->caddr2, __entry->caddr3)
 );
 
 TRACE_EVENT(sched_preempt_disable,
 
-	TP_PROTO(u64 delta, bool irqs_disabled,	unsigned long caddr0,
-			unsigned long caddr1, unsigned long caddr2,
-			unsigned long caddr3, unsigned long caddr4),
+	TP_PROTO(u64 delta, bool irqs_disabled,
+			unsigned long caddr0, unsigned long caddr1,
+			unsigned long caddr2, unsigned long caddr3),
 
-	TP_ARGS(delta, irqs_disabled, caddr0, caddr1, caddr2, caddr3, caddr4),
+	TP_ARGS(delta, irqs_disabled, caddr0, caddr1, caddr2, caddr3),
 
 	TP_STRUCT__entry(
 		__field(u64, delta)
@@ -109,7 +105,6 @@ TRACE_EVENT(sched_preempt_disable,
 		__field(void*, caddr1)
 		__field(void*, caddr2)
 		__field(void*, caddr3)
-		__field(void*, caddr4)
 	),
 
 	TP_fast_assign(
@@ -119,14 +114,12 @@ TRACE_EVENT(sched_preempt_disable,
 		__entry->caddr1 = (void *)caddr1;
 		__entry->caddr2 = (void *)caddr2;
 		__entry->caddr3 = (void *)caddr3;
-		__entry->caddr4 = (void *)caddr4;
 	),
 
-	TP_printk("delta=%llu(ns) irqs_d=%d Callers:(%ps<-%ps<-%ps<-%ps<-%ps)",
+	TP_printk("delta=%llu(ns) irqs_d=%d Callers:(%ps<-%ps<-%ps<-%ps)",
 				__entry->delta, __entry->irqs_disabled,
 				__entry->caddr0, __entry->caddr1,
-				__entry->caddr2, __entry->caddr3,
-				__entry->caddr4)
+				__entry->caddr2, __entry->caddr3)
 );
 
 #endif /* _TRACE_PREEMPTIRQ_H */
