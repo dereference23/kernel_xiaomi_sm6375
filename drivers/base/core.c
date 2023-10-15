935081c7159af2534efbb047754b398c3a881c75
Alexander Winkowski <dereference23@outlook.com>
2023-10-15 04:28:16 +0000

    Revert "sched: Compile cpu_isolated_mask in SCHED_WALT only"
    This reverts commit 9f125adcf2899382a0942a4620d064b02979b516.
    
    Change-Id: I7c06d842dfb869d71d2e4005ad812c7dc9380693
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -1574,12 +1574,10 @@ int trylock_device_hotplug(void)
 	return mutex_trylock(&device_hotplug_lock);
 }
 
-#ifdef CONFIG_SCHED_WALT
 void lock_device_hotplug_assert(void)
 {
 	lockdep_assert_held(&device_hotplug_lock);
 }
-#endif
 
 #ifdef CONFIG_BLOCK
 static inline int device_is_not_partition(struct device *dev)
