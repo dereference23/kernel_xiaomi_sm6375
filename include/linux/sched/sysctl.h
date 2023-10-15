57be7dc1a1a5ee840e90f67e974da4f153dd0886
Alexander Winkowski <dereference23@outlook.com>
2023-10-15 06:05:16 +0000

    Revert "tracing/sched: Clean up preemptoff/irqsoff extensions"
    This reverts commit fcecef89e5b73dee593515e815e02382d8c67e2a.
    
    Change-Id: I575d30bde15108f1870c2b9ebc97ff1d7a934748
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -75,6 +75,16 @@ sched_ravg_window_handler(struct ctl_table *table, int write,
 			loff_t *ppos);
 #endif
 
+#if defined(CONFIG_PREEMPTIRQ_EVENTS) || defined(CONFIG_PREEMPT_TRACER)
+extern unsigned int sysctl_preemptoff_tracing_threshold_ns;
+#endif
+#if defined(CONFIG_PREEMPTIRQ_EVENTS) && defined(CONFIG_IRQSOFF_TRACER)
+extern unsigned int sysctl_irqsoff_tracing_threshold_ns;
+extern unsigned int sysctl_irqsoff_dmesg_output_enabled;
+extern unsigned int sysctl_irqsoff_crash_sentinel_value;
+extern unsigned int sysctl_irqsoff_crash_threshold_ns;
+#endif
+
 enum sched_tunable_scaling {
 	SCHED_TUNABLESCALING_NONE,
 	SCHED_TUNABLESCALING_LOG,
