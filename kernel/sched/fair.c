cc0fc8f194a567af5aeb5f84494fd0148603d390
Alexander Winkowski <dereference23@outlook.com>
2023-10-15 04:28:32 +0000

    Revert "sched/walt: Consolidate WALT parameters"
    This reverts commit e2ac8116912f0a8e236d00cecebd8a3ed5b877e0.
    
    Change-Id: I14393f515ada941530c09e0b99c636497fcb5c5c
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6558,7 +6558,7 @@ unsigned long capacity_curr_of(int cpu)
 #ifdef CONFIG_SCHED_WALT
 static inline bool walt_get_rtg_status(struct task_struct *p)
 {
-	struct walt_related_thread_group *grp;
+	struct related_thread_group *grp;
 	bool ret = false;
 
 	rcu_read_lock();
@@ -6850,7 +6850,7 @@ static inline unsigned long
 cpu_util_next_walt(int cpu, struct task_struct *p, int dst_cpu)
 {
 	unsigned long util =
-		cpu_rq(cpu)->wrq.walt_stats.cumulative_runnable_avg_scaled;
+			cpu_rq(cpu)->walt_stats.cumulative_runnable_avg_scaled;
 	bool queued = task_on_rq_queued(p);
 
 	/*
@@ -8267,8 +8267,7 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 	}
 
 	if (env->flags & LBF_IGNORE_PREFERRED_CLUSTER_TASKS &&
-			 !preferred_cluster(
-				cpu_rq(env->dst_cpu)->wrq.cluster, p))
+			 !preferred_cluster(cpu_rq(env->dst_cpu)->cluster, p))
 		return 0;
 
 	/* Don't detach task if it doesn't fit on the destination */
@@ -8277,7 +8276,7 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 		return 0;
 
 	/* Don't detach task if it is under active migration */
-	if (env->src_rq->wrq.push_task == p)
+	if (env->src_rq->push_task == p)
 		return 0;
 #endif
 
@@ -10593,7 +10592,7 @@ int active_load_balance_cpu_stop(void *data)
 	BUG_ON(busiest_rq == target_rq);
 
 #ifdef CONFIG_SCHED_WALT
-	push_task = busiest_rq->wrq.push_task;
+	push_task = busiest_rq->push_task;
 	target_cpu = busiest_rq->push_cpu;
 	if (push_task) {
 		if (task_on_rq_queued(push_task) &&
@@ -10649,14 +10648,14 @@ int active_load_balance_cpu_stop(void *data)
 out_unlock:
 	busiest_rq->active_balance = 0;
 #ifdef CONFIG_SCHED_WALT
-	push_task = busiest_rq->wrq.push_task;
+	push_task = busiest_rq->push_task;
 #endif
 	target_cpu = busiest_rq->push_cpu;
 	clear_reserved(target_cpu);
 
 #ifdef CONFIG_SCHED_WALT
 	if (push_task)
-		busiest_rq->wrq.push_task = NULL;
+		busiest_rq->push_task = NULL;
 #endif
 
 	rq_unlock(busiest_rq, &rf);
@@ -11360,7 +11359,7 @@ static bool silver_has_big_tasks(void)
 	for_each_possible_cpu(cpu) {
 		if (!is_min_capacity_cpu(cpu))
 			break;
-		if (cpu_rq(cpu)->wrq.walt_stats.nr_big_tasks)
+		if (cpu_rq(cpu)->walt_stats.nr_big_tasks)
 			return true;
 	}
 
