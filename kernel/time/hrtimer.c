145d9d6f3d3a0032a09aca7819e97b620443ca98
Alexander Winkowski <dereference23@outlook.com>
2023-10-15 12:41:03 +0000

    Revert "hrtimer: make sure PINNED flag is cleared after removing hrtimer"
    This reverts commit 55efbbcc5fa1b2fe19e882657f233fd3645ae2d5.
    
    Change-Id: Ic91ee65bb45569f847f4601f898de274f6d8dd74
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -1056,7 +1056,6 @@ remove_hrtimer(struct hrtimer *timer, struct hrtimer_clock_base *base,
 			reprogram &= !keep_local;
 
 		__remove_hrtimer(timer, base, state, reprogram);
-		timer->state &= ~HRTIMER_STATE_PINNED;
 		return 1;
 	}
 	return 0;
@@ -1151,7 +1150,7 @@ static int __hrtimer_start_range_ns(struct hrtimer *timer, ktime_t tim,
 
 	/* Update pinned state */
 	timer->state &= ~HRTIMER_STATE_PINNED;
-	timer->state |= (!!(mode & HRTIMER_MODE_PINNED)) << HRTIMER_PINNED_SHIFT;
+	timer->state |= !!(mode & HRTIMER_MODE_PINNED) << HRTIMER_PINNED_SHIFT;
 
 	first = enqueue_hrtimer(timer, new_base, mode);
 	if (!force_local)
