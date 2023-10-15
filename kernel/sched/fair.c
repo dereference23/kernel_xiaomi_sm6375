033c0ad22d1e03b5b51f1dd759316b99614ddbf9
Alexander Winkowski <dereference23@outlook.com>
2023-10-15 05:48:12 +0000

    Revert "sched/fair: bonus vruntime for task boost 3"
    This reverts commit b6e5c7c0f52ce103516f815873e9bbc3f913be25.
    
    Change-Id: I4891cec21bce01861558c079eabaf7eb661246ef
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4072,9 +4072,6 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int initial)
 			thresh >>= 1;
 
 		vruntime -= thresh;
-		if (entity_is_task(se) && per_task_boost(task_of(se)) ==
-				TASK_BOOST_STRICT_MAX)
-			vruntime -= sysctl_sched_latency;
 	}
 
 	/*
