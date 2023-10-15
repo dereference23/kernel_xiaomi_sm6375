f119e9ea6ed7aed996f43c5ada8aa81de31b1555
Alexander Winkowski <dereference23@outlook.com>
2023-10-15 05:48:12 +0000

    Revert "sched/fair: bonus vruntime for task boost 3"
    This reverts commit b6e5c7c0f52ce103516f815873e9bbc3f913be25.
    
    Change-Id: I4891cec21bce01861558c079eabaf7eb661246ef
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2199,6 +2199,17 @@ unsigned long arch_scale_max_freq_capacity(struct sched_domain *sd, int cpu)
 
 unsigned long capacity_curr_of(int cpu);
 
+#ifdef CONFIG_SMP
+static inline unsigned long capacity_of(int cpu)
+{
+	return cpu_rq(cpu)->cpu_capacity;
+}
+
+static inline unsigned long capacity_orig_of(int cpu)
+{
+	return cpu_rq(cpu)->cpu_capacity_orig;
+}
+
 #ifdef CONFIG_SCHED_WALT
 static inline int per_task_boost(struct task_struct *p)
 {
@@ -2217,17 +2228,6 @@ static inline int per_task_boost(struct task_struct *p)
 	return 0;
 }
 #endif
-
-#ifdef CONFIG_SMP
-static inline unsigned long capacity_of(int cpu)
-{
-	return cpu_rq(cpu)->cpu_capacity;
-}
-
-static inline unsigned long capacity_orig_of(int cpu)
-{
-	return cpu_rq(cpu)->cpu_capacity_orig;
-}
 
 static inline unsigned long task_util(struct task_struct *p)
 {
