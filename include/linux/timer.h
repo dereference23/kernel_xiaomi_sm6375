3261d43abf760682394f58ff484ae5609f5a71ff
Alexander Winkowski <dereference23@outlook.com>
2023-10-15 13:23:24 +0000

    Revert "timer: Add a global deferrable timer"
    This reverts commit 489af2b1266fda38489211184633d01e4329a49d.
    
    Change-Id: I695487ef85b61f50cf6f69e3cbd7a3a743da255c
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- include/linux/timer.h
+++ include/linux/timer.h
@@ -187,8 +187,6 @@ extern void add_timer(struct timer_list *timer);
 
 extern int try_to_del_timer_sync(struct timer_list *timer);
 
-extern struct timer_base timer_base_deferrable;
-
 #if defined(CONFIG_SMP) || defined(CONFIG_PREEMPT_RT)
   extern int del_timer_sync(struct timer_list *timer);
 #else
