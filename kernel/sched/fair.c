0ecee804cc7aebd07444d6afc24ce166e661e846
Alexander Winkowski <dereference23@outlook.com>
2023-10-15 06:38:37 +0000

    Revert "sched/fair: Improve the scheduler"
    This reverts commit c47c3c9ed169028a56558d94da97dad6280e93af.
    
    Change-Id: Ibc77b2c78f6b34e918b343d93ca74afde31d05f1
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3846,7 +3846,7 @@ done:
 }
 
 static inline bool
-bias_to_this_cpu(struct task_struct *p, int cpu, int start_cpu)
+bias_to_waker_cpu(struct task_struct *p, int cpu, int start_cpu)
 {
 	bool base_test = cpumask_test_cpu(cpu, &p->cpus_mask) &&
 						cpu_active(cpu);
@@ -6632,7 +6632,6 @@ enum fastpaths {
 	NONE = 0,
 	SYNC_WAKEUP,
 	PREV_CPU_FASTPATH,
-	MANY_WAKEUP,
 };
 
 static void find_best_target(struct sched_domain *sd, cpumask_t *cpus,
@@ -7210,17 +7209,10 @@ int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu,
 	if (need_idle)
 		sync = 0;
 
-	if (sync && bias_to_this_cpu(p, cpu, start_cpu)) {
+	if (sync && bias_to_waker_cpu(p, cpu, start_cpu)) {
 		best_energy_cpu = cpu;
 		fbt_env.fastpath = SYNC_WAKEUP;
-		goto done;
-	}
-
-	if (is_many_wakeup(sibling_count_hint) && prev_cpu != cpu &&
-				bias_to_this_cpu(p, prev_cpu, start_cpu)) {
-		best_energy_cpu = prev_cpu;
-		fbt_env.fastpath = MANY_WAKEUP;
-		goto done;
+		goto sync_wakeup;
 	}
 
 	rcu_read_lock();
@@ -7407,7 +7399,7 @@ unlock:
 	    (capacity_orig_of(prev_cpu) <= capacity_orig_of(start_cpu)))
 		best_energy_cpu = prev_cpu;
 
-done:
+sync_wakeup:
 	trace_sched_task_util(p, cpumask_bits(candidates)[0], best_energy_cpu,
 			sync, need_idle, fbt_env.fastpath, placement_boost,
 			start_t, boosted, is_rtg, get_rtg_status(p), start_cpu);
