d65cdf49686badd662b6233a5e5ced903838588b
Alexander Winkowski <dereference23@outlook.com>
2023-10-15 09:07:42 +0000

    Revert "kernel: Add snapshot of changes to support cpu isolation"
    This reverts commit 201ea48219ba6a1640aa6bc602ae12e7a211eeb3.
    
    Change-Id: I5e778d81cc46c3f3fd2e899e4014c79cc350e99c
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/time/timer.c
+++ kernel/time/timer.c
@@ -2093,8 +2093,7 @@ static void __migrate_timers(unsigned int cpu, bool remove_pinned)
 		 */
 		forward_timer_base(new_base);
 
-		if (!cpu_online(cpu))
-			BUG_ON(old_base->running_timer);
+		BUG_ON(old_base->running_timer);
 
 		for (i = 0; i < WHEEL_SIZE; i++)
 			migrate_timer_list(new_base, old_base->vectors + i,
@@ -2113,10 +2112,12 @@ int timers_dead_cpu(unsigned int cpu)
 	return 0;
 }
 
+#ifdef CONFIG_CPUSETS
 void timer_quiesce_cpu(void *cpup)
 {
 	__migrate_timers(*(unsigned int *)cpup, false);
 }
+#endif /* CONFIG_CPUSETS */
 
 #endif /* CONFIG_HOTPLUG_CPU */
 
