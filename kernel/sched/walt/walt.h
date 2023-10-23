ab2abe1d52e6eb14e405513bb6413d1a8b246bea
Alexander Winkowski <dereference23@outlook.com>
2023-10-23 18:00:36 +0000

    sched/walt: Compile out cpu-boost
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/sched/walt/walt.h
+++ kernel/sched/walt/walt.h
@@ -206,10 +206,10 @@ static inline unsigned int walt_nr_rtg_high_prio(int cpu)
 
 extern int core_ctl_init(void);
 
-#ifdef CONFIG_CPU_FREQ
+#if defined(CONFIG_SCHED_WALT_ORIG) && defined(CONFIG_CPU_FREQ)
 extern int cpu_boost_init(void);
 #else
-static inline int cpu_boost_init(void) { }
+static inline void cpu_boost_init(void) { }
 #endif
 
 #else /* CONFIG_SCHED_WALT */
