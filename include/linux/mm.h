55be97c34a3959a49751351c8bd455e129fb12c8
Alexander Winkowski <dereference23@outlook.com>
2023-12-16 18:34:11 +0000

    Revert "vmscan: Support multiple kswapd threads per node"
    This reverts commit 767f64715054b9b05836c3a82bba2998f9dd7d96.
    
    Change-Id: I0e860b4771ba345b8d321284790a0afe25e482cb
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2351,7 +2351,6 @@ extern void set_dma_reserve(unsigned long new_dma_reserve);
 extern void memmap_init_zone(unsigned long, int, unsigned long, unsigned long,
 		enum meminit_context, struct vmem_altmap *);
 extern void setup_per_zone_wmarks(void);
-extern void update_kswapd_threads(void);
 extern int __meminit init_per_zone_wmark_min(void);
 extern void mem_init(void);
 extern void __init mmap_init(void);
@@ -2372,7 +2371,6 @@ extern void zone_pcp_update(struct zone *zone);
 extern void zone_pcp_reset(struct zone *zone);
 
 /* page_alloc.c */
-extern int kswapd_threads;
 extern int min_free_kbytes;
 extern int watermark_boost_factor;
 extern int watermark_scale_factor;
@@ -3072,14 +3070,6 @@ static inline int pages_identical(struct page *page1, struct page *page2)
 }
 
 extern int want_old_faultaround_pte;
-
-#ifndef CONFIG_MULTIPLE_KSWAPD
-static inline void update_kswapd_threads_node(int nid) {}
-static inline int multi_kswapd_run(int nid) { return 0; }
-static inline void multi_kswapd_stop(int nid) {}
-static inline void multi_kswapd_cpu_online(pg_data_t *pgdat,
-					const struct cpumask *mask) {}
-#endif /* CONFIG_MULTIPLE_KSWAPD */
 
 /**
  * seal_check_future_write - Check for F_SEAL_FUTURE_WRITE flag and handle it
--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -144,7 +144,6 @@ static int ten_thousand = 10000;
 #ifdef CONFIG_PERF_EVENTS
 static int six_hundred_forty_kb = 640 * 1024;
 #endif
-static int __maybe_unused max_kswapd_threads = MAX_KSWAPD_THREADS;
 
 #ifdef CONFIG_SCHED_WALT
 static int neg_three = -3;
@@ -1894,17 +1893,6 @@ static struct ctl_table vm_table[] = {
 		.proc_handler	= watermark_boost_factor_sysctl_handler,
 		.extra1		= SYSCTL_ZERO,
 	},
-#ifdef CONFIG_MULTIPLE_KSWAPD
-	{
-		.procname	= "kswapd_threads",
-		.data		= &kswapd_threads,
-		.maxlen		= sizeof(kswapd_threads),
-		.mode		= 0644,
-		.proc_handler	= kswapd_threads_sysctl_handler,
-		.extra1		= SYSCTL_ONE,
-		.extra2		= &max_kswapd_threads,
-	},
-#endif
 	{
 		.procname	= "watermark_scale_factor",
 		.data		= &watermark_scale_factor,
