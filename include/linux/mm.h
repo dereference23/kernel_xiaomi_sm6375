a798660ead1b2d7958a26e2a32546a64a51986d9
Alexander Winkowski <dereference23@outlook.com>
2024-01-12 18:06:05 +0000

    Revert "ANDROID: GKI: mm: add struct vm_fault fields for SPECULATIVE_PAGE_FAULTS"
    This reverts commit e89b2b7f5b06da2dbbd064ea17fbdb04a5796a32.
    
    Change-Id: Ia0b405e949e842ae90b69b9d2e169efd39237124
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -464,8 +464,6 @@ struct vm_fault {
 					 * page table to avoid allocation from
 					 * atomic context.
 					 */
-	unsigned long vma_flags;	/* Speculative Page Fault field */
-	pgprot_t vma_page_prot;		/* Speculative Page Fault field */
 	ANDROID_VENDOR_DATA(1);
 	ANDROID_VENDOR_DATA(2);
 };
