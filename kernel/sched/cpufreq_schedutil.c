b6596a2dad8b09fe320fe16f280690fa39b248e6
Alexander Winkowski <dereference23@outlook.com>
2024-05-17 17:22:15 +0000

    cpufreq: schedutil: Disable frequency boosting
    Change-Id: If3ecd671504d113d9ad53816bb83ac18c1b77c2e
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -259,7 +259,7 @@ unsigned long schedutil_cpu_util(int cpu, unsigned long util_cfs,
 	 * frequency will be gracefully reduced with the utilization decay.
 	 */
 	util = util_cfs + cpu_util_rt(rq);
-	if (type == FREQUENCY_UTIL)
+	if (type == FREQUENCY_UTIL && uclamp_rq_util_with(rq, util, p) < util)
 		util = uclamp_rq_util_with(rq, util, p);
 
 	dl_util = cpu_util_dl(rq);
