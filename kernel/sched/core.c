5ea4e944aeb9efa95a24fca926476272c9c7c6c3
Alexander Winkowski <dereference23@outlook.com>
2024-05-19 04:57:02 +0000

    sprd_rotation: Improve clarity
    Change-Id: I3a3e5e08ed857d32652cb6566c07dff2a7308292
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3829,7 +3829,8 @@ void scheduler_tick(void)
 
 #ifdef CONFIG_SPRD_ROTATION_TASK
 	if (curr->sched_class == &fair_sched_class) {
-		if (rq->misfit_task_load && curr->state == TASK_RUNNING) {
+		if (rq->misfit_task_load && curr->state == TASK_RUNNING &&
+		    cpumask_test_cpu(cpu, &min_cap_cpu_mask)) {
 			raw_spin_lock(&rotation_lock);
 			check_for_task_rotation(rq);
 			raw_spin_unlock(&rotation_lock);
