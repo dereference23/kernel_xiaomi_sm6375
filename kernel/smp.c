a52cee27dffe0199c433aaa247e2d1b4e5bf838e
Alexander Winkowski <dereference23@outlook.com>
2024-06-08 16:11:21 +0000

    Revert "smp: Wake up all idle CPUs when suspending to idle"
    This reverts commit ecec86e083d3160b0b5ed158ec9557a6f6898d85.
    
    Change-Id: I6434b7c462da6d79b708791445a2976d8de288b2
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/smp.c
+++ kernel/smp.c
@@ -20,7 +20,6 @@
 #include <linux/sched.h>
 #include <linux/sched/idle.h>
 #include <linux/hypervisor.h>
-#include <linux/suspend.h>
 
 #include "smpboot.h"
 
@@ -792,8 +791,8 @@ void wake_up_all_idle_cpus(void)
 	for_each_online_cpu(cpu) {
 		if (cpu == smp_processor_id())
 			continue;
-		if (s2idle_state == S2IDLE_STATE_ENTER ||
-		    !cpu_isolated(cpu))
+
+		if (!cpu_isolated(cpu))
 			wake_up_if_idle(cpu);
 	}
 	preempt_enable();
