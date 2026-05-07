b82833515886eb6c53b5adc0ad99e212ebe09568
Alexander Winkowski <dereference23@outlook.com>
2026-05-07 20:38:07 +0000

    Revert "ANDROID: fix ABI by undoing atomic64_t -> u64 type conversion"
    This reverts commit 95dcc82b80b73d36f99ddd8ebdd6ec11bad23444.

--- net/core/sock.c
+++ net/core/sock.c
@@ -1546,7 +1546,7 @@ static int sk_getsockopt(struct sock *sk, int level, int optname,
 		lv = sizeof(u64);
 		if (len != lv)
 			return -EINVAL;
-		v.val64 = atomic64_read(&sock_net(sk)->net_cookie);
+		v.val64 = sock_net(sk)->net_cookie;
 		break;
 
 	default:
